f68db53891423fb0c5f3aebdb81e6e6af7f306da
Ryan DeWolfe <ryandewolfe33@gmail.com>
2021-02-20 14:41:36 -0800

    add events list

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calendar from './components/calander';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calendar/>
   </React.StrictMode>,
   document.getElementById('root')
 );
