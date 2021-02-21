4234550fb8c331d2980e8d0190b9d741f76ca169
lis-b <contact@lisab.dev>
2021-02-21 03:32:17 -0800

    brought over Calendar and TextInput components by Ryan

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import TextInput from './TextInput';
+
+
+const styleSheet = {
+    margin: "30px auto",
+    borderRadius:"30px",
+    width: "600px",
+    height:"700px",
+    boxSizing: "border-box",
+    background: "#FEC8D8",
+    padding:"40px",
+    fontFamily:"Open Sans",
+    fontWeight:"bold"
+}
+
+class Calendar extends React.Component {
+    state = {
+        count: 0,
+        events: []
+     }
+
+     constructor(props) {
+         super(props);
+         this.handleNewEvent = this.handleNewEvent.bind(this);
+         this.renderEvents = this.renderEvents.bind(this);
+     }
+
+     handleNewEvent(time, eventName) {
+        this.setState({events: this.state.events.concat([[time, eventName]]).sort(function(a,b){
+            if( a[0][0] === b[0][0] ) {
+                if( a[0][1] === b[0][1]) return 0;
+                else return a[0][1] < b[0][1]? -1:1;}
+            return a[0][0] < b[0][0]? -1:1;
+        }),
+                       count: this.state.count + 1});
+
+     }
+
+     renderEvents(){
+         if(this.state.count === 0) return <p>No events scheduled today</p>;
+
+         return <ul style={{padding:"25px"}}>{this.state.events.map(item => (<li sytle={{padding:"20px"}} key={item}>{item[0][0]+':'+item[0][1] + '        '+ item[1]}</li>))}</ul>;
+     }
+
+     renderCount(){
+         var business = "";
+         if(this.state.count === 0)
+            business = "Take the day off!";
+        else if(this.state.count <5)
+            business = "A normal relaxing day.";
+        else
+            business = "Wow thats a lot of events!";
+        return <p style={{textAlign:"center"}}>{this.state.count} events today. {business}</p>
+     }
+
+    render() {
+        return (
+            <div style={styleSheet} className="module">
+                <h1 style={{color:"#875C36", textAlign:"center"}}> Today's Calendar</h1>
+                <span>{this.renderCount()}</span>
+                {this.renderEvents()}
+                <TextInput  handleNewEvent = {this.handleNewEvent}/>
+
+            </div>
+
+            )
+    }
+
+
+}
+
+export default Calendar;
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+class TextInput extends React.Component {
+    state = {  }
+
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        var time = [this.hour.value, this.minute.value];
+        this.props.handleNewEvent(time, this.eventName.value);
+        this.hour.value = '';
+        this.minute.value = '';
+        this.eventName.value = '';
+
+    }
+    render() {
+        return (
+            <div style={{textAlign:"center", top:"650px", position:"absolute"}}>
+            <form onSubmit={this.handleSubmit}>
+                <label1>
+                    Time (24hr)
+                    <input
+                        style = {{width:"40px"}}
+                        type='number'
+                        name= "Time"
+                        min='0'
+                        max='23'
+                        ref={(hour) => this.hour = hour}
+                        required
+                        />
+                </label1>
+                <label3>
+                    :
+                    <input
+                        style = {{width:"20px"}}
+                        type='text'
+                        name= ":"
+                        min='0'
+                        max ='59'
+                        ref={(minute) => this.minute = minute}
+                        required
+                        />
+                </label3>
+                <label2 style={{padding:"15px"}}>
+                    <span style={{padding:"5px"}}>Event Name:</span>
+                    <input
+                        style ={{width:"150px"}}
+                        type='text'
+                        name='Event Name'
+                        ref={(eventName) => this.eventName = eventName}
+                        required
+                        />
+                </label2>
+                <button style={{borderRadius:"7px"}} type = 'submit'>Add Event</button>
+            </form>
+            </div>
+         );
+    }
+}
+
+export default TextInput;
