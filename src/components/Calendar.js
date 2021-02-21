30cc5f3ba74ff00b54b55ebbacdff69efe9e2960
lis-b <contact@lisab.dev>
2021-02-21 07:25:32 -0800

    Calendar styled

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,6 +3,14 @@ import TextInput from './TextInput';
 
 import './Calendar.sass';
 
+const numberFormat = str => {
+  if (str <= 9) {
+    return '0'+str*1;
+  } else {
+    return str*1;
+  }
+}
+
 class Calendar extends React.Component {
     state = {
         count: 0,
@@ -29,7 +37,13 @@ class Calendar extends React.Component {
      renderEvents(){
          if(this.state.count === 0) return <p>No events scheduled today.</p>;
 
-         return <ul style={{padding:"25px"}}>{this.state.events.map(item => (<li sytle={{padding:"20px"}} key={item}>{item[0][0]+':'+item[0][1] + '        '+ item[1]}</li>))}</ul>;
+         return <div className="items">
+           {this.state.events.map(item => (<>
+             <div className="item-time">
+               {numberFormat(item[0][0])+':'+numberFormat(item[0][1])}
+             </div>
+             <div className="item-name">{item[1]}</div></>))}
+         </div>;
      }
 
      renderCount(){
@@ -40,13 +54,13 @@ class Calendar extends React.Component {
             business = "A normal relaxing day.";
         else
             business = "Wow thats a lot of events!";
-        return <p style={{textAlign:"center"}}>{this.state.count === 0 ? '' : `${`${this.state.count} event${this.state.count === 1 ? '' : 's'}`} today. `}{business}</p>
+        return <p className="center">{this.state.count === 0 ? '' : `${`${this.state.count} event${this.state.count === 1 ? '' : 's'}`} today. `}{business}</p>
      }
 
     render() {
         return (
             <div className="module calendar">
-                <h1>Today's Calendar</h1>
+                <h1>Today's Events</h1>
                 <span>{this.renderCount()}</span>
                 {this.renderEvents()}
                 <TextInput  handleNewEvent = {this.handleNewEvent}/>
--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Article from "./Article";
-import "./NewsBox.scss";
+import "./NewsBox.sass";
 
 const newsAPImain = "http://newsapi.org/v2/top-headlines?";
 const apiKey = `&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`;
@@ -21,8 +21,9 @@ class NewsBox extends React.Component {
             }],
             message: null,
             error: null,
-            category: "",
-            country: "",
+            category: "general",
+            country: "ca",
+            isLoaded: false,
         };
         this.handleCategoryChange = this.handleCategoryChange.bind(this);
         this.handleCountryChange = this.handleCountryChange.bind(this);
@@ -55,11 +56,13 @@ class NewsBox extends React.Component {
                         totalResults: result["totalResults"],
                         articles: result["articles"],
                         message: result["message"],
+                        isLoaded: true,
                     });
                 },
                 (error) => {
                     this.setState({
                         error: error,
+                        isLoaded: true,
                     });
                 }
             );
@@ -67,7 +70,9 @@ class NewsBox extends React.Component {
 
     componentDidMount() {
         let request;
-        request = new Request(newsAPImain + "country=ca" + apiKey);
+        let category = this.state.category !== '' ? "&category=" + this.state.category : '';
+        let country = "country=" + this.state.country;
+        request = new Request(newsAPImain + country + category + apiKey);
         fetch(request)
             .then(res => res.json())
             .then(
@@ -77,11 +82,13 @@ class NewsBox extends React.Component {
                         totalResults: result["totalResults"],
                         articles: result["articles"],
                         message: result["message"],
+                        isLoaded: true,
                     });
                 },
                 (error) => {
                     this.setState({
                         error: error,
+                        isLoaded: true,
                     });
                 }
             );
@@ -89,96 +96,104 @@ class NewsBox extends React.Component {
 
 
     render() {
-        const {articles, error, status, category, country} = this.state;
+        const { articles, error, status, category, country, isLoaded } = this.state;
 
         if (error != null || status === "error") {
             return (
-                <div className = "error">
+                <div className = "module news error">
                     Something went wrong: {this.state.message}
                 </div>
             );
+        } else if (!isLoaded) {
+          return (
+              <div className = "module news loading">
+                  Loading...
+              </div>
+          );
         } else {
             return (
                 <div className="module news">
+                  <h1 className="title">Current Headlines</h1>
+                  <div className="selection">
                     <form className="category" onSubmit={this.handleSubmit}>
-                    <label className="category">
-                            Category: &nbsp;
-                        <select value={category} onChange={this.handleCategoryChange}>
-                            <option value="business">business</option>
-                            <option value="entertainment">entertainment</option>
-                            <option value="general">general</option>
-                            <option value="health">health</option>
-                            <option value="science">science</option>
-                            <option value="sports">sports</option>
-                            <option value="technology">technology</option>
-                        </select>
-                    </label>
-                        <input className="category" type="submit" value="Submit" />
-                    </form>
+                      <label className="category">
+                              <b>Category:</b>
+                          <select value={category} onChange={this.handleCategoryChange}>
+                              <option value="business">Business</option>
+                              <option value="entertainment">Entertainment</option>
+                              <option value="general">General</option>
+                              <option value="health">Health</option>
+                              <option value="science">Science</option>
+                              <option value="sports">Sports</option>
+                              <option value="technology">Technology</option>
+                          </select>
+                      </label>
+                          <button className="category" type="submit" value="Submit">Submit</button>
+                      </form>
 
-                    <form className="country" onSubmit={this.handleSubmit}>
-                        <label className="country">
-                            Country: &nbsp;
-                            <select value={country} onChange={this.handleCountryChange}>
-                                <option value="ae">United Arab Emirates</option>
-                                <option value="ar"> Argentina </option>
-                                <option value="at"> Austria </option>
-                                <option value="au"> Australia </option>
-                                <option value="be"> Belgium </option>
-                                <option value="bg"> Bulgaria </option>
-                                <option value="br"> Brazil </option>
-                                <option value="ca">Canada</option>
-                                <option value="ch"> Switzerland </option>
-                                <option value="cn"> China </option>
-                                <option value="co"> Colombia </option>
-                                <option value="cu"> Cuba </option>
-                                <option value="cz"> Czechia </option>
-                                <option value="de"> Germany </option>
-                                <option value="eg"> Egypt </option>
-                                <option value="fr"> France </option>
-                                <option value="gb"> UK </option>
-                                <option value="gr"> Greece </option>
-                                <option value="hk"> Hong Kong </option>
-                                <option value="hu"> Hungary </option>
-                                <option value="id"> Indonesia </option>
-                                <option value="ie"> Ireland </option>
-                                <option value="il"> Israel </option>
-                                <option value="in"> India </option>
-                                <option value="it"> Italy </option>
-                                <option value="jp"> Japan </option>
-                                <option value="kr"> Korea </option>
-                                <option value="lt"> Lithuania </option>
-                                <option value="lv"> Latvia </option>
-                                <option value="ma"> Morocco </option>
-                                <option value="mx"> Mexico </option>
-                                <option value="my"> Malaysia </option>
-                                <option value="ng"> Nigeria </option>
-                                <option value="nl"> Netherlands </option>
-                                <option value="no"> Norway </option>
-                                <option value="nz"> New Zealand </option>
-                                <option value="ph"> Philippines </option>
-                                <option value="pl"> Poland </option>
-                                <option value="pt"> Portugal </option>
-                                <option value="ro"> Romania </option>
-                                <option value="rs"> Serbia </option>
-                                <option value="ru"> Russia </option>
-                                <option value="sa"> Saudi Arabia </option>
-                                <option value="se"> Sweden </option>
-                                <option value="sg"> Singapore </option>
-                                <option value="si"> Slovenia </option>
-                                <option value="sk"> Slovakia </option>
-                                <option value="th"> Thailand </option>
-                                <option value="tr"> Turkey </option>
-                                <option value="tw"> Taiwan </option>
-                                <option value="ua"> Ukraine </option>
-                                <option value="us"> US </option>
-                                <option value="ve"> Venezuela  </option>
-                                <option value="za"> South Africa </option>
-                            </select>
-                        </label>
-                        <input type="submit" value="Submit" />
-                    </form>
-
+                      <form className="country" onSubmit={this.handleSubmit}>
+                          <label className="country">
+                              <b>Country:</b>
+                              <select value={country} onChange={this.handleCountryChange}>
+                                  <option value="ar"> Argentina </option>
+                                  <option value="at"> Austria </option>
+                                  <option value="au"> Australia </option>
+                                  <option value="be"> Belgium </option>
+                                  <option value="bg"> Bulgaria </option>
+                                  <option value="br"> Brazil </option>
+                                  <option value="ca">Canada</option>
+                                  <option value="cn"> China </option>
+                                  <option value="co"> Colombia </option>
+                                  <option value="cu"> Cuba </option>
+                                  <option value="cz"> Czechia </option>
+                                  <option value="de"> Germany </option>
+                                  <option value="eg"> Egypt </option>
+                                  <option value="fr"> France </option>
+                                  <option value="gr"> Greece </option>
+                                  <option value="hk"> Hong Kong </option>
+                                  <option value="hu"> Hungary </option>
+                                  <option value="id"> Indonesia </option>
+                                  <option value="ie"> Ireland </option>
+                                  <option value="il"> Israel </option>
+                                  <option value="in"> India </option>
+                                  <option value="it"> Italy </option>
+                                  <option value="jp"> Japan </option>
+                                  <option value="kr"> Korea </option>
+                                  <option value="lt"> Lithuania </option>
+                                  <option value="lv"> Latvia </option>
+                                  <option value="ma"> Morocco </option>
+                                  <option value="mx"> Mexico </option>
+                                  <option value="my"> Malaysia </option>
+                                  <option value="ng"> Nigeria </option>
+                                  <option value="nl"> Netherlands </option>
+                                  <option value="nz"> New Zealand </option>
+                                  <option value="no"> Norway </option>
+                                  <option value="ph"> Philippines </option>
+                                  <option value="pl"> Poland </option>
+                                  <option value="pt"> Portugal </option>
+                                  <option value="ro"> Romania </option>
+                                  <option value="ru"> Russia </option>
+                                  <option value="sa"> Saudi Arabia </option>
+                                  <option value="rs"> Serbia </option>
+                                  <option value="sg"> Singapore </option>
+                                  <option value="si"> Slovenia </option>
+                                  <option value="sk"> Slovakia </option>
+                                  <option value="se"> Sweden </option>
+                                  <option value="ch"> Switzerland </option>
+                                  <option value="th"> Thailand </option>
+                                  <option value="tr"> Turkey </option>
+                                  <option value="tw"> Taiwan </option>
+                                  <option value="ua"> Ukraine </option>
+                                  <option value="ae"> United Arab Emirates</option>
+                                  <option value="gb"> United Kingdom </option>
+                                  <option value="us"> United States of America </option>
+                                  <option value="ve"> Venezuela  </option>
+                                  <option value="za"> South Africa </option>
+                              </select>
+                          </label>
+                          <button type="submit" value="Submit">Submit</button>
+                      </form>
+                    </div>
 
                     <div className="articles">
                         {articles.map((article) => (<Article key={article["url"]} article={article} />))}
