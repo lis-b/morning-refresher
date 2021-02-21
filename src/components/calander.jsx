68cb406ddbee974e5d36e373fcb589c64ae746c7
Ryan DeWolfe <ryandewolfe33@gmail.com>
2021-02-20 20:07:14 -0800

    Events display earliest to latest in the day

--- src/components/calander.jsx
+++ src/components/calander.jsx
@@ -14,7 +14,12 @@ class Calander extends Component {
      }
 
      handleNewEvent(time, eventName) {
-        this.setState({events: this.state.events.concat([[time, eventName]]),
+        this.setState({events: this.state.events.concat([[time, eventName]]).sort(function(a,b){
+            if( a[0][0] == b[0][0] ) {
+                if( a[0][1] == b[0][1]) return 0;
+                else return a[0][1] < b[0][1]? -1:1;}
+            return a[0][0] < b[0][0]? -1:1;
+        }),
                        count: this.state.count + 1});
         
      }
