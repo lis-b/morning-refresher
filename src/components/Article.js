31efce5a4a751863be0ae5c9bb8d9044f49874e1
lis-b <contact@lisab.dev>
2021-02-21 10:37:08 -0800

    fixed Windows-based bugs, added better scrollbar than default

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,7 +4,7 @@ import { ReactComponent as LinkSVG } from '../svg/Feather/external-link.svg';
 
 export class Article extends React.Component {
     render() {
-        const { description, publishedAt, title, url, urlToImage} = this.props.article;
+        const { description, publishedAt, title, url, urlToImage } = this.props.article;
         const date = new Date(publishedAt);
 
         return (
@@ -15,7 +15,7 @@ export class Article extends React.Component {
                 <p className="pub">{date.toLocaleDateString()} </p>
 
                 {urlToImage !== null ?
-                <center><img src={urlToImage} alt="{title}" /></center> : ''
+                <center><img src={urlToImage} alt={title} /></center> : ''
                 }
 
                 <p className="desc"> {description} </p>
